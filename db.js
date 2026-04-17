f9a0992367912d2fe25889fb84a670dee1a2487c
aliatwood <aliatwood0104@gmail.com>
2026-04-17 23:08:09 +0200

    update db.js
    Ändrade variabelnamn till samma som railway

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQLPORT
 });
 
