0011535d7d673a1732935c47619d421700cfcc6a
aliatwood <aliatwood0104@gmail.com>
2026-04-17 21:12:02 +0200

    Skapade inserts.sql
    Skapade inserts.sql och la in fiktiva värden

---
+++ inserts.sql
@@ -0,0 +1,8 @@
+/* Inmatning för workexperience */
+
+insert into workexperience(id, companyName, jobTitle, location, startDate, endDate, description) values
+(1, "Sfmovie", "cashier", "Linkoping", "2015-01-05", "2025-02-05", "Took on customers"),
+(2, "Mainlibrary", "booksorter", "Norrkoping", "2016-02-07", "2026-03-07", "Sorted books in order"),
+(3, "Mcdonalds", "crewmember", "Stockholm", "2017-03-09", "2017-04-09", "Made food from scratch"),
+(4, "Coop", "stocker", "Sundsvall", "2018-04-11", "2018-05-11", "Sorted and stocked food on different shelves"),
+(5, "Liu", "Substituteteacher", "Uppsala", "2019-05-13", "2019-06-13", "Taught students when teacher was unavailable");
