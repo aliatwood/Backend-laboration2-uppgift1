415aedb0fee51f898bcbe606d92812fbdaf090bb
aliatwood <aliatwood0104@gmail.com>
2026-04-17 19:55:38 +0200

    update server.js
    skapar en server med express, cors och dotenv

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log("Servern körs på " + port);
+});
+
+
 
