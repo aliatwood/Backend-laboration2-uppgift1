f3c4d9931186cddc96ddade2a30e7a8f961d90cc
aliatwood <aliatwood0104@gmail.com>
2026-04-17 20:03:43 +0200

    Skapar en db.js
    Skapar databasuppkoppling mot MariaDB

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+db.connect((err) =>{
+    if (err){
+        console.error("Databasfel: " + err);
+        return;
+    }
+    console.log("Ansluten till databasen!");
+});
+
+module.exports = db;
