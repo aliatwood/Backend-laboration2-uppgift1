36c06881788593cb174277e133e25c5edc35d088
aliatwood <aliatwood0104@gmail.com>
2026-04-19 16:33:25 +0200

    update server.js
    skapar tabellen om den ej finns och lägger in testdata om den e tom

--- server.js
+++ server.js
@@ -10,6 +10,28 @@ const arbetserfarenheter = require("./arbetserfarenheter");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Skapar tabellen om den ej finns
+db.query(`CREATE TABLE IF NOT EXISTS workexperience (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    companyName VARCHAR(30) NOT NULL,
+    jobTitle VARCHAR(25) NOT NULL,
+    location VARCHAR(30) NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE,
+    description VARCHAR(200) NOT NULL
+)`);
+
+// Lägger in testdata om tabellen är tom
+db.query(`INSERT INTO workexperience (companyName, jobTitle, location, startDate, endDate, description)
+    SELECT * FROM (
+        SELECT 'Sfmovie','cashier','Linkoping','2015-01-05','2025-02-05','Took on customers' UNION ALL
+        SELECT 'Mainlibrary','booksorter','Norrkoping','2016-02-07','2026-03-07','Sorted books in order' UNION ALL
+        SELECT 'Mcdonalds','crewmember','Stockholm','2017-03-09','2017-04-09','Made food from scratch' UNION ALL
+        SELECT 'Coop','stocker','Sundsvall','2018-04-11','2018-05-11','Sorted and stocked food on different shelves' UNION ALL
+        SELECT 'Liu','Substituteteacher','Uppsala','2019-05-13','2019-06-13','Taught students when teacher was unavailable'
+    ) AS tmp
+    WHERE NOT EXISTS (SELECT 1 FROM workexperience) LIMIT 5`);
+
 // Middleware för cors och json
 app.use(cors());
 app.use(express.json());
